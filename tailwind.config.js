06e2c0e1f8eeb6bfb49d00e4ab622cc6dfc75be4
sefadalklc <sefadalk11@gmail.com>
2023-03-06 15:04:13 +0300

    Header.jsx is created

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode:"jit",
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      backgroundColor: theme => ({
+        "brand-color" : "#5d3ebc"
+      })
+    },
+  },
+  plugins: [],
+}
