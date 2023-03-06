11785262d4bb086885737a800927fd8af6edf6f0
sefadalklc <sefadalk11@gmail.com>
2023-03-06 23:26:11 +0300

    HeroSection add photo

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Slider from "react-slick";
+
 
 const HeroSection = () => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
-    <div>HeroSection</div>
+    <div className=' '>
+  
+    <Slider {...settings}>
+      <div>
+        <img className='w-full h-[500px] object-cover' src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-2.jpg" alt="" />
+      </div>
+      <div>
+        <img className='w-full h-[500px] object-cover' src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-4.jpg" alt="" />
+      </div>
+      
+    </Slider>
+  </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: theme => ({
-        "brand-color" : "#5d3ebc"
+        "brand-color" : "#4c3398",
+        "primary-brand-color" : "#5d3ebc",
+        "secondary-brand-color" : "#7849f7",
       })
     },
   },
