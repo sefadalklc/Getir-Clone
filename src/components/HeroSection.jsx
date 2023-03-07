10d71c10b1d8739e98452364f3f929007ae098c4
sefadalklc <sefadalk11@gmail.com>
2023-03-07 12:47:36 +0300

    HeroSection left side created

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
     slidesToScroll: 1
   };
   return (
-    <div className=' '>
+    <div className='relative before:bg-gradient-to-r before:from-primary-brand-color  before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10'>
   
     <Slider {...settings}>
       <div>
@@ -23,6 +23,17 @@ const HeroSection = () => {
       </div>
       
     </Slider>
+    <div className='container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20'>
+        <div className='flex flex-col gap-y-8'>
+        <img height={180} width={180}
+						     src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg"
+						     alt=""/>
+
+            <h3 className="text-4xl font-semibold tracking-tighter	 text-white">
+							Dakikalar içinde <br/> kapınızda
+						</h3>
+        </div>
+    </div>
   </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
         "brand-color" : "#4c3398",
         "primary-brand-color" : "#5d3ebc",
         "secondary-brand-color" : "#7849f7",
-      })
+      }),
+      gradientColorStops: theme => ({
+        "brand-color" : "#4c3398",
+        "primary-brand-color" : "#5d3ebc",
+        "secondary-brand-color" : "#7849f7",
+      }),
     },
   },
   plugins: [],
