f99d721a556127175a55be433d425dea09717593
sefadalklc <sefadalk11@gmail.com>
2023-03-24 16:52:46 +0300

    Favorites section completed

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import x from "../api/products.json"
+import Category from './ui/Category';
+import { useState,useEffect } from 'react';
+import ProductItem from './ui/ProductItem';
 
 const Favorites = () => {
+
+  const [products,setProducts] = useState([])
+
+  useEffect(() => {
+    setProducts(x)
+  }, [])
+
+
   return (
-    <div>Favorites</div>
+    <div>
+      <h3 className='text-sm font-semibold mb-3'>Favoriler</h3>
+      <div className="grid grid-cols-8 gap-0.1px bg-white rounded-lg overflow-hidden">
+            {!x.length && "Yükleniyor..."}
+          {products.length && products.map(product => <ProductItem key={product.id} product={product} />)}
+          </div>
+    </div>
   )
 }
 
